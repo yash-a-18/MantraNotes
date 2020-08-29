455635fc7fd5e783692f3aab8199d901c8483f1f
yash-a-18 <yashamethiya2001@gmail.com>
2020-08-29 17:23:42 +0530

    Title added

--- js/app.js
+++ js/app.js
@@ -7,15 +7,21 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     //   console.log(notesObj);
     showNotes();
 });
@@ -33,8 +39,9 @@ function showNotes() {
         html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title">${index + 1}.  ${element.title}</h5>
+                        <hr>
+                        <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
                 </div>`;
@@ -43,7 +50,7 @@ function showNotes() {
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
     } else {
-        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section to add notes.`;
     }
 }
 
@@ -72,7 +79,8 @@ search.addEventListener("input", function () {
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if (cardTxt.includes(inputVal)) {
+        let titleTxt = element.getElementsByTagName("h5")[0].innerText;
+        if (cardTxt.includes(inputVal) || titleTxt.includes(inputVal)) {
             element.style.display = "block";
         }
         else {
